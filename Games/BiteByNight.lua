f59637b6224b09b8b36c30d627fc1b84f62943b9
fatalxxx1 <deedewz12@gmail.com>
2026-04-10 13:43:50 +0700

    Update BiteByNight.lua

--- Games/BiteByNight.lua
+++ Games/BiteByNight.lua
@@ -3,20 +3,40 @@
 
 local Players = game:GetService('Players')
 local LocalPlayer = Players.LocalPlayer
+local PlayerGui = LocalPlayer.PlayerGui
+
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-
-
-
 local loop = LoopManager().new
+
 AutoGen = Legit:CreateToggle({
     Name = "Auto Gen",
-	Animation = false,
-    StartingState = true,
     Callback = function(state) 
-       
        if state then
-        loop:AddTask('sw')
+            loop:AddTask('AutoGen',function()
+                
+                local Gui = PlayerGui:FindFirstChild("Gen")
+		
+                if Gui then
+					local eventPath = PlayerGui:FindFirstChild("Gen"):FindFirstChild("GeneratorMain"):FindFirstChild("Event")
+
+                    wait(4)
+		            local args = {	
+    	            {
+                     Wires = true,
+                     Switches = true,
+                     Lever = true
+    	            }
+	             }
+
+                eventPath:FireServer(unpack(args))
+               Gui:Destroy()
+                end
+            end)
+        else
+            loop:Destroy()
+            loop = LoopManager.new()
        end
     end
+
 })
