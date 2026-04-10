65c71ec0aaf6d32222f54fdaf2fcaa9f2ca2eb56
fatalxxx1 <deedewz12@gmail.com>
2026-04-10 13:38:53 +0700

    Update BiteByNight.lua

--- Games/BiteByNight.lua
+++ Games/BiteByNight.lua
@@ -9,3 +9,14 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 
 local loop = LoopManager().new
+AutoGen = Legit:CreateToggle({
+    Name = "Auto Gen",
+	Animation = false,
+    StartingState = true,
+    Callback = function(state) 
+       
+       if state then
+        loop:AddTask('sw')
+       end
+    end
+})
