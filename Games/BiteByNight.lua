cb60c4ca6dd8ba31c8d68233aa353b382fe5a07a
fatalxxx1 <deedewz12@gmail.com>
2026-04-10 13:31:14 +0700

    Update BiteByNight.lua

--- Games/BiteByNight.lua
+++ Games/BiteByNight.lua
@@ -3,5 +3,9 @@
 
 local Players = game:GetService('Players')
 local LocalPlayer = Players.LocalPlayer
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+
+
 
 local loop = LoopManager().new
