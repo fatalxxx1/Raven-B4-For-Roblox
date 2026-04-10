efbeb3aa4531e358228bf9a7bce4e527838e2995
fatalxxx1 <deedewz12@gmail.com>
2026-04-10 13:18:07 +0700

    Create Bite By Nightfunctions.lua

---
+++ Functions/Bite By Nightfunctions.lua
@@ -0,0 +1,30 @@
+local LoopManager = {}
+LoopManager.__index = LoopManager
+
+function LoopManager.new()
+    return setmetatable({tasks = {}}, LoopManager)
+end
+
+function LoopManager:AddTask(id, callback, delay, priority)
+    if self.tasks[id] then self:RemoveTask(id) end
+    self.tasks[id] = {
+        callback = callback,
+        connection = RunService.Heartbeat:Connect(function(dt)
+            pcall(callback, dt)
+        end)
+    }
+end
+
+function LoopManager:RemoveTask(id)
+    if self.tasks[id] then
+        self.tasks[id].connection:Disconnect()
+        self.tasks[id] = nil
+    end
+end
+
+function LoopManager:Destroy()
+    for id in pairs(self.tasks) do self:RemoveTask(id) end
+end
+
+
+
