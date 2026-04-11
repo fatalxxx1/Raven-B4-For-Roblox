da9e76be717d1d71cff913e7098d6d902fb972be
fatalxxx1 <deedewz12@gmail.com>
2026-04-11 11:25:35 +0700

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Near-B4/Raven-B4-For-Roblox/refs/heads/main/Raven%20B4%20Loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fatalxxx1/Raven-B4-For-Roblox/refs/heads/main/Raven%20B4%20Loader.lua"))()
